3c2f04551fae4088dc9e00f54e9e4d5b5bfbeff5
cogilvy <chrisogilvy@cox.net>
2019-06-14 17:58:44 -0400

    stuff

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,7 +23,7 @@ class ApplicationController < ActionController::API
     end
   end
 
-  def current_user
+  def session_user
     if decoded_token
       user_id = decoded_token[0]['user_id']
       @user = User.find_by(id: user_id)
--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,9 @@ Rails.application.routes.draw do
         resources :likes, only: [:index, :new, :create, :destroy]
         resources :comments, only: [:index, :new, :create, :destroy]
 
-        post '/login', to: 'auth#create'
+        post '/login', to: 'auth#login'
         get '/profile', to: 'users#profile'
+        get "/auto_login", to: "auth#auto_login"
       end
   end
 end
