db3c4b866fecf353cfcf0d51817527acfe29f903
cogilvy <chrisogilvy@cox.net>
2019-06-15 16:57:52 -0400

    auth2

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -1,12 +1,13 @@
 class Api::V1::AuthController < ApplicationController
-  skip_before_action :authorized, only: [:create]
+  skip_before_action :authorized, only: [:login]
 
   def login
+    byebug
     @user = User.find_by(username: user_login_params[:username])
     #User#authenticate comes from BCrypt
     if @user && @user.authenticate(user_login_params[:password])
       # encode token comes from ApplicationController
-      token = encode_token({ user_id: @user.id })
+      token = encode_token({ user_id: @user.id }, 'my_s3cr3t')
       render json: { user: UserSerializer.new(@user), jwt: token }, status: :accepted
     else
       render json: { errors: 'Invalid username or password' }, status: :unauthorized
